cf2e6b58ed6783891c70cec52a600649bb9aee30
Miguel Núñez Galán <miguelnunezgalan@Air-de-Miguel.technicolor.net>
2023-12-04 16:33:09 +0100

    completed

---
+++ queries.js
@@ -0,0 +1,104 @@
+db.collection.find({ type: "Bird" });
+db.collection.find({ found_in_countries: "Australia" });
+db.collection.find({ conservation_status: "Critically Endangered" });
+db.collection.find({ diet: "Herbivore", habitat: "Forest" });
+db.collection.find([
+  {
+    $addFields: {
+      "physical_characteristics.weight": {
+        $toDouble: {
+          $replaceAll: {
+            input: "$physical_characteristics.weight",
+            find: " kg",
+            replacement: "",
+          },
+        },
+      },
+    },
+  },
+  {
+    $match: {
+      "physical_characteristics.weight": {
+        $gt: 400,
+      },
+    },
+  },
+]);
+db.collection.find([
+  {
+    $addFields: {
+      "physical_characteristics.height": {
+        $toDouble: {
+          $replaceAll: {
+            input: "$physical_characteristics.height",
+            find: " m",
+            replacement: "",
+          },
+        },
+      },
+    },
+  },
+  {
+    $match: {
+      "physical_characteristics.height": {
+        $lt: 1,
+      },
+    },
+  },
+]);
+db.collection.find({ found_in_countries: "China" });
+db.collection.find({ diet: "Omnivore", conservation_status: "Endangered" });
+db.collection.find({ common_name: { $regex: "^Lion" } });
+db.collection.find([
+  {
+    $addFields: {
+      "physical_characteristics.length": {
+        $toDouble: {
+          $replaceAll: {
+            input: "$physical_characteristics.length",
+            find: " m",
+            replacement: "",
+          },
+        },
+      },
+    },
+  },
+  {
+    $match: {
+      "physical_characteristics.length": {
+        $gt: 2,
+      },
+    },
+  },
+]);
+db.collection.find({ "found_in_countries.": { $regex: "Africa" } });
+db.collection.find({ scientific_name: { $regex: "^A" } });
+db.collection.find([
+  {
+    $match: {
+      diet: "Carnivore",
+    },
+  },
+  {
+    $addFields: {
+      "physical_characteristics.weight": {
+        $toDouble: {
+          $replaceAll: {
+            input: "$physical_characteristics.weight",
+            find: " kg",
+            replacement: "",
+          },
+        },
+      },
+    },
+  },
+  {
+    $match: {
+      "physical_characteristics.weight": {
+        $lt: 50,
+      },
+    },
+  },
+]);
+db.collection.find({ conservation_status: "Vulnerable", habitat: "Desert" });
+db.collection.find({ common_name: { $regex: "Tiger" } });
